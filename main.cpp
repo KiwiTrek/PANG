bff1b96e74959d8f0235bcadd326463ee6a1ff98
Theran1 <addg98@gmail.com>
2020-03-27 16:49:04 +0100

    Doing main a library things

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#pragma once
+#include <stdio.h>
+#include <iostream>
+
+#include "SDL/include/SDL.h"
+#include "SDL_Image/include/SDL_image.h"
+#include "SDL_Mixer/include/SDL_mixer.h"
+
+#pragma comment(lib,"SDL/libx86/SDL2.lib")
+#pragma comment(lib,"SDL/libx86/SDL2main.lib")
+#pragma comment(lib,"SDL_Image/libx86/SDL2_image.lib")
+#pragma comment(lib,"SDL_Mixer/libx86/SDL2_mixer.lib")
+
+//en mi ordenador me da un bug raro al intentar darle play, intentadlo vosotros para ver si es mi tortadora o es que algo hice mal
+
+
+
+int main(int argc, char* argv[]) {
+
+    system("pause");
+    return 0;
+}
