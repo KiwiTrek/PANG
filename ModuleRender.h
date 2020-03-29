9bd7a2f89e69b38db71241aeb951199b806c9a60
Lladruc <59049906+Lladruc37@users.noreply.github.com>
2020-03-29 17:29:45 +0200

    Render Module

---
+++ ModuleRender.h
@@ -0,0 +1,42 @@
+#ifndef __MODULE_RENDER_H__
+#define __MODULE_RENDER_H__
+#include "Module.h"
+
+struct SDL_Rect;
+struct SDL_Texture;
+struct SDL_Renderer;
+
+class ModuleRender : public Module {
+public:
+    ModuleRender();
+    ~ModuleRender();
+
+    // Creates the render
+    bool Init() override;
+
+    // Resets the render
+    update_status PreUpdate() override;
+
+    // Updates the screen with the rendered content
+    update_status PostUpdate() override;
+
+    // Called on application exit.
+    // Destroys the rendering context
+    bool CleanUp() override;
+
+    // Prints a texture to the rendering context.
+    // Param texture	- A valid SDL Texture, validation checks are not performed
+    // Param x,y		- Position x,y in the screen (upper left axis)
+    // Param section	- The portion of the texture we want to copy. nullptr for the entire texture
+    bool Blit(SDL_Texture* texture, int x, int y, SDL_Rect* section = nullptr);
+
+public:
+    // Rendering context used for any rendering action
+    SDL_Renderer* renderer = nullptr;
+
+private:
+    SDL_Texture* testTexture = nullptr;
+
+};
+
+#endif //__MODULE_RENDER_H__
