a33bfaede745c9a1b505b6187a3577d4f96ea3f2
Theran1 <addg98@gmail.com>
2020-04-03 19:55:32 +0200

    main loopen
    how a man with a toater fights with a simple piece of code and almost looses

--- main.cpp
+++ main.cpp
@@ -11,9 +11,64 @@
 #pragma comment(lib,"SDL_Mixer/libx86/SDL2_mixer.lib")
 
 
+enum class MAIN_STATES
+{
+	MAIN_CREATION,
+	MAIN_START,
+	MAIN_UPDATE,
+	MAIN_FINISH,
+	MAIN_EXIT
+};
 
-int main(int argc, char* argv[]) {
+int main() {
+	ReportMemoryLeaks();
 
-    system("pause");
-    return 0;
+	int main_return = EXIT_FAILURE;
+	MAIN_STATES state = MAIN_STATES::MAIN_CREATION;
+	Game* game = nullptr;
+
+	while (state != MAIN_STATES::MAIN_EXIT) {
+		switch (state) {
+		case MAIN_STATES::MAIN_CREATION: {
+			LOG("Game Creation --------------\n");
+			game = new Game();
+			state = MAIN_STATES::MAIN_START;
+		}	
+		break;
+
+		case MAIN_STATES::MAIN_START: {
+			LOG("Game Start --------------\n");
+			if (game->Init() == false) {
+				LOG("Game Init exits with error -----\n");
+				state = MAIN_STATES::MAIN_EXIT;
+			}
+			else {
+				state = MAIN_STATES::MAIN_UPDATE;
+			}
+		}	
+		break;
+
+		case MAIN_STATES::MAIN_UPDATE: {
+			update_status status = game->Update();
+
+			if (status == update_status::UPDATE_ERROR) {
+				LOG("Game Update exits with error -----\n");
+				state = MAIN_STATES::MAIN_EXIT;
+			}
+			else if (status == update_status::UPDATE_STOP) {
+				state = MAIN_STATES::MAIN_FINISH;
+			}
+		}
+		break;
+
+		// TODO 1: Implement case MAIN_FINISH
+		// Remember to quit the game with EXIT_SUCCESS if everything went well :)
+		}
+	}
+
+	LOG("\nBye :)\n");
+
+	// TODO 6: Remove ALL memory leaks
+
+	return main_return;
 }
