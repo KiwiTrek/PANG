6f2579a5ba34d765a21e563871a24ad895d59f09
Weedzard <59049803+TitoLuce@users.noreply.github.com>
2020-05-11 20:24:49 +0200

    .

--- ModuleTileset.cpp
+++ ModuleTileset.cpp
@@ -20,7 +20,7 @@ bool ModuleTileset::Init() {
 //By now we will consider all modules to be permanently active
 bool ModuleTileset::Start() {
     if (game->GetModuleLevelOne()->IsEnabled) {
-        game->GetModuleTextures()->Load("Resources/Sprites/Foreground1.png");
+		foreground = game->GetModuleTextures()->Load("Resources/Sprites/Foreground1.png");
         for (int i = 0; i != 26; ++i) {
             for (int j = 0; j != 48; ++j) {
                 switch (level[i][j]) {
@@ -53,6 +53,17 @@ UPDATE_STATUS ModuleTileset::PreUpdate() { return UPDATE_STATUS::UPDATE_CONTINUE
 //Called at the middle of each application loop
 UPDATE_STATUS ModuleTileset::Update() { return UPDATE_STATUS::UPDATE_CONTINUE; }
 //Called at the end of each application loop
-UPDATE_STATUS ModuleTileset::PostUpdate() { return UPDATE_STATUS::UPDATE_CONTINUE; }
+UPDATE_STATUS ModuleTileset::PostUpdate() { 
+	for (int y = 0; y < 26; y++) {
+		for (int x = 0; x < 48; x++) {
+			if (level[y][x] > 1) {
+				SDL_Rect eightBit = { x * TILE_SIZE,y * TILE_SIZE,TILE_SIZE,TILE_SIZE };
+				game->GetModuleRender()->Blit(foreground, x * TILE_SIZE, y * TILE_SIZE, false, &eightBit);
+			}
+		}
+	}
+
+	return UPDATE_STATUS::UPDATE_CONTINUE; 
+}
 // Called on application exit.
 bool ModuleTileset::CleanUp() { return true; }
